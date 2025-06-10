644d29117a7c7ceacfb99a9cb21b9d7133f5bb61
Andrew Benyeogor Osenwe <Benyeogorosenwe@gmail.com>
2025-06-10 09:21:01 -0500

    errors corrected-use client was missing in some page components

--- src/components/resources/PhishingDetectionLab.jsx
+++ src/components/resources/PhishingDetectionLab.jsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { useState, useEffect } from 'react';
 import { getRandomChallenges, getBalancedChallenges, getChallengesByCategory, getChallengeStats } from '@/data/phishingChallenges';
 
